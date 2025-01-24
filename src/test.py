ea8f26a66b9db452dc41c16c4388ce7a31a57048
Martí Climent <marticlilop@gmail.com>
2025-01-24 20:39:20 +0100

    Add tests for program ranking

--- src/test.py
+++ src/test.py
@@ -17,9 +17,15 @@ versions = ["ver1", "ver1", "ver2", "ver1", "ver1", "ver2", "ver1"]
 activeManagers = [0x0001, 0x0002, 0x0003, 0x0001, 0x0003, 0x0003, 0x0004]
 activeVersions = [0x0001, 0x0002, 0x0001, 0x0803, 0x0804, 0x0001, 0x0003]
 
+progrs = ["prog1", "prog2", "prog3", "prog2", "prog1", "prog1", "prog1"]
+manags = ["M1", "M1", "M1", "M1", "M1", "M1", "M2"]
+sourcs = ["source1", "source2", "source1", "source1", "source1", "source1", "source1"]
+
+
 
 process = Popen(f"bun {os.getcwd()}\\server.ts --data-folder ./tests-data".split(" "), shell=False, cwd=os.getcwd())
 
+errors = 0
 
 try:
     for i in range(7):
@@ -37,6 +43,22 @@ try:
         
     print("Users registered")
 
+
+    for i in range(7):
+        print(f"Installing program for user  user {i+1}...")
+        url = "http://localhost:3000/install"
+        headers = {
+            "clientId": users[i],
+            "packageId": progrs[i],
+            "managerName": manags[i],
+            "sourceName": sourcs[i],
+        }
+
+        response = requests.post(url, headers=headers, json={})
+        print("Result:", response.status_code)
+
+
+
     print("Fetching report...")
     report_url = "http://localhost:3000/report"
     report_headers = {
@@ -45,30 +67,48 @@ try:
 
     report_response = requests.get(report_url, headers=report_headers)
     report_data = report_response.json()
+    
+
 
     if(report_data["active_users"] != 6):
         print("❌ active_users had an unexpected value")
+        errors += 1
     
     if(int(report_data["active_versions"]["ver1"]) != 83):
         print(f"❌ active_versions.ver1 had an unexpected value {int(report_data["active_versions"]["ver1"])}")
+        errors += 1
         
     if(int(report_data["active_versions"]["ver2"]) != 16):
         print(f"❌ active_versions.ver2 had an unexpected value {int(report_data["active_versions"]["ver2"])}")
+        errors += 1
     
     managerVal = [66.66666666666667, 50, 16.666666666666668, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for i in range(len(managerVal)):
         if(report_data["active_managers"][i] != managerVal[i]):
             print(f"❌ active_managers[{i}] had an unexpected value {report_data["active_managers"][i]} (expected was {managerVal[i]})")
+            errors += 1
     
    
     settingsVal = [66.66666666666667, 50, 16.666666666666668, 0, 0, 0, 0, 0, 0, 0, 0, 33.333333333333336, 0, 0, 0, 0]
     for i in range(len(settingsVal)):
         if(report_data["active_settings"][i] != settingsVal[i]):
             print(f"❌ active_settings[{i}] had an unexpected value {report_data["active_settings"][i]} (expected was {settingsVal[i]})")
+            errors += 1
     
-   
+    expectedRanking = [['prog1', 'M1', 'source1', 3], ['prog2', 'M1', 'source2', 1], ['prog3', 'M1', 'source1', 1], ['prog2', 'M1', 'source1', 1], ['prog1', 'M2', 'source1', 1]]
+    
+    for i in range(len(expectedRanking)):
+        element = report_data["program_ranking"][i]
+        for j in range(len(element)):
+            if element[j] != expectedRanking[i][j]:
+                print(f"❌ program_ranking[{i}] with value {element[j]} does not equal expected value {expectedRanking[i][j]}")        
+                errors += 1
         
-    print("✅ Tests finished")
+    print("---------------------------------")
+    if errors == 0: 
+        print("✅ Tests passed")
+    else: 
+        print(f"❌ Tests failed with {errors} failures")
     
 
     print("Report data:", report_data)
@@ -81,3 +121,5 @@ except Exception as e:
     
 finally:
     process.kill()
+    
+exit(errors)
